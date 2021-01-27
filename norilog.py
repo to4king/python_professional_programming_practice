96c6d5bdf76e13cc754bcd689024f7a6100d4115
to4king <to4king@gmail.com>
2021-01-27 23:42:24 +0900

    02-06-04まで終了（テンプレート化、静的ファイルパス変更など）

--- norilog.py
+++ norilog.py
@@ -1,5 +1,10 @@
 import json
 
+from flask import Flask, render_template, request, redirect
+from datetime import datetime
+
+application = Flask(__name__)
+
 DATA_FILE = 'norilog.json'
 
 
@@ -15,30 +20,58 @@ def save_data(start, finish, memo, created_at):
     :type created_at: datetime.datetime
     :return None
     """
-    with open(DATA_FILE, mode="r", encoding="utf-8") as f:
-        try:
-            # json モジュールでデータベースファイルを開きます
-            database = json.load(f)
-        except FileNotFoundError:
-            database = []
+    try:
+        # json モジュールでデータベースファイルを開きます
+        database = json.load(open(DATA_FILE, mode="r", encoding="utf-8"))
+    except FileNotFoundError:
+        database = []
 
-        database = {
-            "start": start,
-            "finish": finish,
-            "memo": memo,
-            "created_at": created_at.strftime("%Y-%m-%d %H:%M")
-        }
+    database.insert(0, {
+        "start": start,
+        "finish": finish,
+        "memo": memo,
+        "created_at": created_at.strftime("%Y-%m-%d %H:%M")
+    })
+    database.close()
 
-        json.dump(database, open(DATA_FILE, mode="w", encoding="utf-8"), indent=4, ensure_ascii=False)
+    json.dump(database, open(DATA_FILE, mode="w", encoding="utf-8"), indent=4, ensure_ascii=False)
 
 
 def load_data():
     """記録データを返す"""
-    with open(DATA_FILE, mode="r", encoding="utf-8") as f:
-        try:
-            # json モジュールでデータベースファイルを開きます
-            database = json.load(f)
-        except FileNotFoundError:
-            database = []
-        return database
+    try:
+        # json モジュールでデータベースファイルを開きます
+        database = json.load(open(DATA_FILE, mode="r", encoding="utf-8"))
+    except FileNotFoundError:
+        database = []
+    return database
 
+
+@application.route('/')
+def index():
+    """トップページ
+    テンプレートを使用してページを表示します
+    """
+    # 記録されているデータを読み込む
+    rides = load_data()
+    return render_template('index.html', rides=rides)
+
+
+@application.route('/save', methods=['POST'])
+def save():
+    """記録用 URL"""
+    # 記録用データを取得する
+    start = request.form.get('start')  # 出発
+    finish = request.form.get('finish')  # 到着
+    memo = request.form.get('memo')  # メモ
+    created_at = datetime.now()  # 記録時間（現在時間）を保存します
+
+    save_data(start, finish, memo, created_at)
+
+    # 保存後はトップページにリダイレクトします。
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    # IPアドレス0.0.0.0の8000番ポートでアプリケーションを実行します
+    application.run('0.0.0.0', 8000, debug=True)
